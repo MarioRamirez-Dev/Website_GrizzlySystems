b40bbe72a6188836492e00a8d61770a15ffdd408
MarioRamirez-dev <mario11842@hotmail.com>
2020-12-24 01:49:49 -0600

    Menú con Jquery funcionando correctamente y de igual manera el apartado de los servicios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,10 @@
 // Desarrollado por:
 // Grizzly Systems
+var elementTop = $('.menu').offset().top;
+$(window).scroll(function(){
+    if($(window).scrollTop() > elementTop){
+        $('body').addClass('menu_fixed');
+    }else{ 
+        $('body').removeClass('menu_fixed');
+    }
+});
