4fe16d482c0a0536ce6812ab8dea08059f63fe9f
MarioRamirez-dev <mario11842@hotmail.com>
2020-12-30 20:35:59 -0600

    Implementación de contenido en Includes con su respectivo estilo asignado, faltando hacer lo mismo en el Include de la joyería.

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,13 @@ $(window).scroll(function(){
         $('body').removeClass('menu_fixed');
     }
 });
+
+//Slider Portafolio
+var counter = 1;
+setInterval(function(){
+document.getElementById('radio' + counter).checked = true;
+counter++;
+if(counter > 4){
+    counter = 1;
+}
+}, 3000);
